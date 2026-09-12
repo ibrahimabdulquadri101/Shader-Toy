625cb14044fc98bbe8c71a3538b680a7dea3398f
ibrahim <ibrahimabdulquadri446@gmail.com>
2026-09-12 23:50:25 +0100

    Add window

---
+++ src/Window.cpp
@@ -0,0 +1,101 @@
+#include "Window.h"
+
+#include <iostream>
+#include <string>
+
+bool Window::init(int width, int height, const std::string& title)
+{
+    this->width = width;
+    this->height = height;
+    this->title = title;
+
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return false;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    handle = glfwCreateWindow(width, height, this->title.c_str(), nullptr, nullptr);
+    if (!handle)
+    {
+        std::cerr << "Error occurred opening your window" << std::endl;
+        glfwTerminate();
+        return false;
+    }
+
+    glfwMakeContextCurrent(handle);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        glfwDestroyWindow(handle);
+        glfwTerminate();
+        return false;
+    }
+
+    glViewport(0, 0, width, height);
+    return true;
+}
+
+bool Window::shouldClose()
+{
+    return handle && glfwWindowShouldClose(handle);
+}
+
+void Window::swapBuffers()
+{
+    if (handle)
+    {
+        glfwSwapBuffers(handle);
+    }
+}
+
+void Window::pollEvents()
+{
+    glfwPollEvents();
+}
+
+void Window::getMousePosition(double& x, double& y)
+{
+    if (!handle)
+    {
+        x = 0.0;
+        y = 0.0;
+        return;
+    }
+
+    double xpos = 0.0;
+    double ypos = 0.0;
+    glfwGetCursorPos(handle, &xpos, &ypos);
+    x = xpos;
+    y = ypos;
+}
+
+bool Window::isKeyPressed(int key)
+{
+    return handle && glfwGetKey(handle, key) == GLFW_PRESS;
+}
+
+int Window::getWidth()
+{
+    return width;
+}
+
+int Window::getHeight()
+{
+    return height;
+}
+
+void Window::terminate()
+{
+    if (handle)
+    {
+        glfwDestroyWindow(handle);
+        handle = nullptr;
+    }
+    glfwTerminate();
+}
--- src/Window.h
+++ src/Window.h
@@ -1,4 +1,28 @@
 #ifndef WINDOW_H
 #define WINDOW_H
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <string>
+
+class Window
+{
+private:
+    GLFWwindow* handle;
+    int width;
+    int height;
+    std::string title;
+
+public:
+    bool init(int width, int height, const std::string& title);
+    bool shouldClose();
+    void swapBuffers();
+    void pollEvents();
+    void getMousePosition(double& x, double& y);
+    bool isKeyPressed(int key);
+    int getWidth();
+    int getHeight();
+    void terminate();
+};
+
 #endif // WINDOW_H
